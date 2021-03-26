d9daad6b85f087f5b02a60b2e3b718c82f34d929
Benjamin Marwell <bmarwell@gmail.com>
2021-03-26 09:23:57 +0100

    pin influx to 1.8, update grafana dashboard, use batch-compatible curl options, use debian instead of alpine for speetest, refine container names, change default speedtest timeout from 5 to 30, add .gitignore file.

--- speedtest/scripts/init_test_connection.sh
+++ speedtest/scripts/init_test_connection.sh
@@ -1,17 +1,38 @@
-#!/bin/sh
+#!/bin/bash
+set -euo pipefail
+
 FILE="/app/speedtest/test_connection.log"
+# --include
+# --no-progress-meter instead of --silent, not available in debian buster (has curl 7.64 but needs 7.67)
+CURLOPTS=(
+  "--silent"
+  "--fail"
+  "--location"
+  "--max-time" 1
+  "--retry" 1
+  "--retry-delay" 1
+)
+TEST_INTERVAL=${TEST_INTERVAL:-5}
 
 while true 
 do 
-	TIMESTAMP=$(date '+%s')
+  TIMESTAMP=$(date '+%s')
 
-	/app/speedtest/speedtest-cli > $FILE
+  # this is speedtest.py, not the binary from speedtest.net
+  /app/speedtest/speedtest-cli --json --secure > "$FILE"
+  # options for speedtest.net binary
+  #/app/speedtest/speedtest-cli --format=json-pretty --progress=no > "$FILE"
 
-	DOWNLOAD=$(cat $FILE | grep "Download:" | awk -F " " '{print $2}')
-	UPLOAD=$(cat $FILE | grep "Upload:" | awk -F " " '{print $2}')
-	echo "Download: $DOWNLOAD Upload: $UPLOAD    $TIMESTAMP"
-	curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "download,host=local value=$DOWNLOAD"
-	curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "upload,host=local value=$UPLOAD"
-	sleep $TEST_INTERVAL
-
+  PING="$(jq -r '.ping' < "$FILE")"
+  UPLOAD="$(jq -r '.upload' < "$FILE")"
+  DOWNLOAD="$(jq -r '.download' < "$FILE")"
+  # for speedtest.net
+  #PING="$(jq -r '.ping.latency' < "$FILE")"
+  #DOWNLOAD="$(jq -r '.upload.bandwidth' < "$FILE")"
+  #UPLOAD="$(jq -r '.download.bandwidth' < "$FILE")"
+  echo "Download: $DOWNLOAD Upload: $UPLOAD Ping: $PING   $TIMESTAMP"
+  curl "${CURLOPTS[@]}" 'http://db:8086/write?db=speedtest' --data-binary "download,host=local value=$DOWNLOAD"
+  curl "${CURLOPTS[@]}" 'http://db:8086/write?db=speedtest' --data-binary "upload,host=local value=$UPLOAD"
+  curl "${CURLOPTS[@]}" 'http://db:8086/write?db=speedtest' --data-binary "ping,host=local value=$PING"
+  sleep ${TEST_INTERVAL}
 done
